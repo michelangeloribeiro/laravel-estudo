c9f870cb5d55aed5178bb2a1930b0c22863c7b47
Michelangelo Ribeiro <michel@agenciatreis.com.br>
2022-12-22 14:18:50 -0300

    Entendendo Rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, seja bem vindo ao nosso curso.';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre Nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });

b391dc4ea07c756923451474e3787b9cf689d0f4
Michelangelo Ribeiro <michel@agenciatreis.com.br>
2022-12-23 15:04:06 -0300

    comentarios e blocos php

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,29 @@
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}" title="Principal">Principal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}" title="Sobre Nós">Sobre Nós</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}" title="Contato">Contato</a>
+    </li>
+    <li>
+        <a href="{{ route('app.clientes') }}" title="Clientes">Clientes</a>
+    </li>
+    <li>
+        <a href="{{ route('app.fornecedores') }}" title="Fornecedores">Fornecedores</a>
+    </li>
+    <li>
+        <a href="{{ route('app.produtos') }}" title="Produtos">Produtos</a>
+    </li>
+</ul>
+
+<h3>Fornecedor</h3>
+
+{{-- gerando comentario --}}
+
+@php
+ //Teset
+ /* teste */   
+@endphp

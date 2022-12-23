e48ebf120ed66cd2391a68a0a802607d70e393c9
Michelangelo Ribeiro <michel@agenciatreis.com.br>
2022-12-23 14:36:00 -0300

    parametros controlador view

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,7 +8,12 @@ class TesteController extends Controller
 {
 
     public function teste(int $p1, int $p2){
-        echo $p1 + $p2;
+
+        //return view('site.teste', ['p1' => $p1, 'p2' => $p2]); // array associativo
+        
+        //return view('site.teste', compact('p1','p2')); //compact
+
+        return view('site.teste')->with('p1', $p1)->with('p2',$p2); //with
     }
 
 }
---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,23 @@
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}" title="Principal">Principal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}" title="Sobre Nós">Sobre Nós</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}" title="Contato">Contato</a>
+    </li>
+    <li>
+        <a href="{{ route('app.clientes') }}" title="Clientes">Clientes</a>
+    </li>
+    <li>
+        <a href="{{ route('app.fornecedores') }}" title="Fornecedores">Fornecedores</a>
+    </li>
+    <li>
+        <a href="{{ route('app.produtos') }}" title="Produtos">Produtos</a>
+    </li>
+</ul>
+
+<h3>Teste</h3>
+{{ $p1 + $p2 }}

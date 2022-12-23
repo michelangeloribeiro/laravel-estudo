90452395004e8335f0a95af7377be4292ec17680
Michelangelo Ribeiro <michel@agenciatreis.com.br>
2022-12-23 14:01:07 -0300

    redirecionamento

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -8,6 +8,15 @@
     <li>
         <a href="{{ route('site.contato') }}" title="Contato">Contato</a>
     </li>
+    <li>
+        <a href="{{ route('app.clientes') }}" title="Clientes">Clientes</a>
+    </li>
+    <li>
+        <a href="{{ route('app.fornecedores') }}" title="Fornecedores">Fornecedores</a>
+    </li>
+    <li>
+        <a href="{{ route('app.produtos') }}" title="Produtos">Produtos</a>
+    </li>
 </ul>
 
 <h3>Principal</h3>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -8,8 +8,16 @@
     <li>
         <a href="{{ route('site.contato') }}" title="Contato">Contato</a>
     </li>
+    <li>
+        <a href="{{ route('app.clientes') }}" title="Clientes">Clientes</a>
+    </li>
+    <li>
+        <a href="{{ route('app.fornecedores') }}" title="Fornecedores">Fornecedores</a>
+    </li>
+    <li>
+        <a href="{{ route('app.produtos') }}" title="Produtos">Produtos</a>
+    </li>
 </ul>
-
 
 <h3>Sobre Nós</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mollis placerat aliquet. Praesent ac ligula et enim commodo faucibus. Curabitur porttitor sem diam, rhoncus egestas lacus molestie id. Suspendisse mi velit, facilisis sit amet pretium in, elementum et leo. Morbi dapibus id purus quis iaculis. Morbi sapien dui, tincidunt non pharetra id, sollicitudin sed ante. Nulla convallis dictum viverra. Praesent euismod lobortis lacus a molestie. Donec iaculis varius urna, id pharetra neque pulvinar et. Aliquam id justo mollis, vestibulum nunc et, euismod erat.</p>
